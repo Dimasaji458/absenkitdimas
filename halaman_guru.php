54a9ab3bfe200b8a733f461f46345aaf2b1c5c15
13 Dimas Genta Aji Saputra <70433400+Dimasaji458@users.noreply.github.com>
2020-08-29 17:51:46 +0700

    Add files via upload

---
+++ halaman_guru.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Guru</title>
+</head>
+<body bgcolor="FF B6 C1">
+	<?php 
+	session_start();
+
+	// cek apakah yang mengakses halaman ini sudah login
+	if($_SESSION['level']==""){
+		header("location:index.php?pesan=gagal");
+	}
+
+	?>
+	<h1>Halaman Guru</h1>
+
+	<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
+	<h4>Tabel Guru</h4>
+	<TABLE BORDER = "1">
+		<TR><TH>No</TH><TH>nama_guru</TH><TH>mapel</TH></TR>
+		<TR><TD>1</TD><TD>andi</TD><TH>PAI</TH></TR>
+		<TR><TD>2</TD><TD>doni</TD><TH>TIK</TH></TR>
+		<TR><TD>3</TD><TD>wahyu</TD><TH>PAI</TH></TR>
+		<TR><TD>4</TD><TD>robert</TD><TH>IPA</TH></TR>
+		<TR><TD>5</TD><TD>kaka</TD><TH>TIK</TH></TR>
+</TABLE>
+<h4>Tabel Siswa</h4>
+	<TABLE BORDER = "1">
+		<TR><TH>No</TH><TH>nama_siswa</TH><TH>jurusan</TH></TR>
+		<TR><TD>1</TD><TD>andi</TD><TH>tkj</TH></TR>
+		<TR><TD>2</TD><TD>doni</TD><TH>tkj</TH></TR>
+		<TR><TD>3</TD><TD>wahyu</TD><TH>tkj</TH></TR>
+		<TR><TD>4</TD><TD>robert</TD><TH>tkj</TH></TR>
+		<TR><TD>5</TD><TD>kaka</TD><TH>tkj</TH></TR>
+</TABLE>
+<br>
+	<a href="logout.php">LogOut</a>
+
+	<br/>
+	<br/>
+</body>
+</html>
---
+++ user.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Waktu pembuatan: 04. Maret 2020 jam 05:07
+-- Versi Server: 5.5.16
+-- Versi PHP: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `multi_user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+(2, 'Septian Adi Nugroho', 'septian', '5b3bb3e5458e02aa356f2fc671ae08d9', 'guru'),
+(3, 'Dimas Genta Aji Saputra', 'dimas', '7d49e40f4b3d8f68c19406a58303f826', 'siswa');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
